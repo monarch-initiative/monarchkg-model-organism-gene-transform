ae0867f83ec3423295ab0fee1fdace3c89075842
Daniel Korn <dkorn@ht1.cluster>
2025-02-07 10:57:25 -0500

    Initial commit

---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# Model Organism Gene Transform Report
+
+{{ get_nodes_report() }}
+
+{{ get_edges_report() }}
---
+++ scripts/generate-rdf.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from kgx.cli.cli_utils import transform as kgx_transform
+from loguru import logger
+
+logger.info(f"Creating rdf output: output/n/a_transform.nt.gz ...")
+
+src_files = []
+src_nodes = f"output/n/a_transform_nodes.tsv"
+src_edges = f"output/n/a_transform_edges.tsv"
+
+if Path(src_nodes).is_file():
+    src_files.append(src_nodes)
+if Path(src_edges).is_file():
+    src_files.append(src_edges)
+
+kgx_transform(
+    inputs=src_files,
+    input_format="tsv",
+    stream=True,
+    output=f"output/n/a_transform.nt.gz",
+    output_format="nt",
+    output_compression="gz",
+)

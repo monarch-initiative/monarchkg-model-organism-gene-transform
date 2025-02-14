ea9feb11d2c46e5a946bd489d1c36082fdc6e100
Daniel Korn <dkorn@ht1.cluster>
2025-02-14 12:58:48 -0500

    Updated name in reporting functions.

--- scripts/generate-rdf.py
+++ scripts/generate-rdf.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from kgx.cli.cli_utils import transform as kgx_transform
 from loguru import logger
 
-logger.info(f"Creating rdf output: output/n/a_transform.nt.gz ...")
+logger.info(f"Creating rdf output: output/model_organism_gene_transform.nt.gz ...")
 
 src_files = []
-src_nodes = f"output/n/a_transform_nodes.tsv"
-src_edges = f"output/n/a_transform_edges.tsv"
+src_nodes = f"output/model_organism_gene_transform_nodes.tsv"
+src_edges = f"output/model_organism_gene_transform_edges.tsv"
 
 if Path(src_nodes).is_file():
     src_files.append(src_nodes)
@@ -18,7 +18,7 @@ kgx_transform(
     inputs=src_files,
     input_format="tsv",
     stream=True,
-    output=f"output/n/a_transform.nt.gz",
+    output=f"output/model_organism_gene_transform.nt.gz",
     output_format="nt",
     output_compression="gz",
 )
